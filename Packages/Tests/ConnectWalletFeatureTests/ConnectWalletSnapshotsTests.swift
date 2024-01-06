b554c71a1632f42a00404bafe703f694a47472f4
Hilal Hakani <hilalalhakani@gmail.com>
2024-01-07 01:12:00 +0200

    test

--- Packages/Tests/ConnectWalletFeatureTests/ConnectWalletSnapshotsTests.swift
+++ Packages/Tests/ConnectWalletFeatureTests/ConnectWalletSnapshotsTests.swift
@@ -1,91 +1,91 @@
-import ComposableArchitecture
-import ConnectWalletFeature
-import Foundation
-import SnapshotTesting
-import XCTest
-
-@MainActor
-final class ConnectWalletSnapshotsTests: XCTestCase {
-    func test_connectWallet_light() async {
-        let connectWalletView = ConnectWalletView(
-            store: .init(
-                initialState: .init(),
-                reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
-        )
-            .environment(\.colorScheme, .light)
-            .environment(\.locale, .init(identifier: "en"))
-
-        assertSnapshot(
-            of: connectWalletView,
-            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
-    }
-
-    func test_connectWallet_dark() async {
-        let connectWalletView = ConnectWalletView(
-            store: .init(
-                initialState: .init(),
-                reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
-        )
-            .environment(\.colorScheme, .dark)
-            .environment(\.locale, .init(identifier: "fr"))
-
-        assertSnapshot(
-            of: connectWalletView,
-            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
-    }
-
-      func test_connectWallet_alert_light() async {
-        let store = StoreOf<ConnectWalletReducer>(
-          initialState: ConnectWalletReducer.State(),
-          reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
-    
-        let connectWalletView  = ConnectWalletView(store: store)
-          .environment(\.colorScheme, .light)
-          .environment(\.locale, .init(identifier: "en"))
-    
-        store.send(.onButtonSelect(.coinbase))
-    
-        assertSnapshot(
-          of: connectWalletView,
-          as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe))
-        )
-      }
-
-      func test_connectWallet_alert_dark() async {
-    
-        let store = StoreOf<ConnectWalletReducer>(
-          initialState: ConnectWalletReducer.State(),
-          reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
-    
-        let connectWalletView = ConnectWalletView(store: store)
-          .environment(\.colorScheme, .dark)
-          .environment(\.locale, .init(identifier: "fr"))
-    
-        store.send(.onButtonSelect(.coinbase))
-    
-        assertSnapshot(
-          of: connectWalletView,
-          as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
-      }
-
-    func test_connectingWallet_light() async {
-        let connectWalletView = ConnectingWalletView(selectedWallet: .metamask, cancelPressed: {})
-            .environment(\.colorScheme, .light)
-            .environment(\.locale, .init(identifier: "en"))
-
-        assertSnapshot(
-            of: connectWalletView,
-            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
-    }
-
-    func test_connectingWallet_dark() async {
-        let connectWalletView = ConnectingWalletView(selectedWallet: .metamask, cancelPressed: {})
-            .environment(\.colorScheme, .dark)
-            .environment(\.locale, .init(identifier: "fr"))
-
-        assertSnapshot(
-            of: connectWalletView,
-            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
-    }
-
-}
+//import ComposableArchitecture
+//import ConnectWalletFeature
+//import Foundation
+//import SnapshotTesting
+//import XCTest
+//
+//@MainActor
+//final class ConnectWalletSnapshotsTests: XCTestCase {
+//    func test_connectWallet_light() async {
+//        let connectWalletView = ConnectWalletView(
+//            store: .init(
+//                initialState: .init(),
+//                reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
+//        )
+//            .environment(\.colorScheme, .light)
+//            .environment(\.locale, .init(identifier: "en"))
+//
+//        assertSnapshot(
+//            of: connectWalletView,
+//            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
+//    }
+//
+//    func test_connectWallet_dark() async {
+//        let connectWalletView = ConnectWalletView(
+//            store: .init(
+//                initialState: .init(),
+//                reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
+//        )
+//            .environment(\.colorScheme, .dark)
+//            .environment(\.locale, .init(identifier: "fr"))
+//
+//        assertSnapshot(
+//            of: connectWalletView,
+//            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
+//    }
+//
+//      func test_connectWallet_alert_light() async {
+//        let store = StoreOf<ConnectWalletReducer>(
+//          initialState: ConnectWalletReducer.State(),
+//          reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
+//    
+//        let connectWalletView  = ConnectWalletView(store: store)
+//          .environment(\.colorScheme, .light)
+//          .environment(\.locale, .init(identifier: "en"))
+//    
+//        store.send(.onButtonSelect(.coinbase))
+//    
+//        assertSnapshot(
+//          of: connectWalletView,
+//          as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe))
+//        )
+//      }
+//
+//      func test_connectWallet_alert_dark() async {
+//    
+//        let store = StoreOf<ConnectWalletReducer>(
+//          initialState: ConnectWalletReducer.State(),
+//          reducer: { ConnectWalletReducer().dependency(\.analyticsClient, .consoleLogger) })
+//    
+//        let connectWalletView = ConnectWalletView(store: store)
+//          .environment(\.colorScheme, .dark)
+//          .environment(\.locale, .init(identifier: "fr"))
+//    
+//        store.send(.onButtonSelect(.coinbase))
+//    
+//        assertSnapshot(
+//          of: connectWalletView,
+//          as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
+//      }
+//
+//    func test_connectingWallet_light() async {
+//        let connectWalletView = ConnectingWalletView(selectedWallet: .metamask, cancelPressed: {})
+//            .environment(\.colorScheme, .light)
+//            .environment(\.locale, .init(identifier: "en"))
+//
+//        assertSnapshot(
+//            of: connectWalletView,
+//            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
+//    }
+//
+//    func test_connectingWallet_dark() async {
+//        let connectWalletView = ConnectingWalletView(selectedWallet: .metamask, cancelPressed: {})
+//            .environment(\.colorScheme, .dark)
+//            .environment(\.locale, .init(identifier: "fr"))
+//
+//        assertSnapshot(
+//            of: connectWalletView,
+//            as: .image(perceptualPrecision: 0.98, layout: .device(config: .iPhoneSe)))
+//    }
+//
+//}
