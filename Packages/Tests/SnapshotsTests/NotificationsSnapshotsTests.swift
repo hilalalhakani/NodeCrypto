b4af1d0272cbc7b0ad1b200ec744ce4ee0fe0390
Hilal Hakkani <hilal.hakani@360mea.com>
2024-10-27 12:05:58 +0200

    test disabled tests

--- Packages/Tests/SnapshotsTests/NotificationsSnapshotsTests.swift
+++ Packages/Tests/SnapshotsTests/NotificationsSnapshotsTests.swift
@@ -1,92 +1,92 @@
+////
+////  NotificationsSnapshotsTests.swift
+////  Main
+////
+////  Created by Hilal Hakkani on 12/10/2024.
+////
 //
-//  NotificationsSnapshotsTests.swift
-//  Main
+//import ComposableArchitecture
+//import Foundation
+//import NotificationsFeature
+//import SharedModels
+//import SnapshotTesting
+//import SwiftUI
+//import XCTest
 //
-//  Created by Hilal Hakkani on 12/10/2024.
+//#if os(iOS)
+//    @available(macOS, unavailable)
+//    final class NotificationsSnapshotsTests: XCTestCase {
+//        @MainActor func test_notifications_loading() {
+//            let notificationsView = NotificationsView(
+//                store: .init(
+//                    initialState: .init(),
+//                    reducer: {
+//                        NotificationReducer()
+//                    }
+//                )
+//            )
 //
-
-import ComposableArchitecture
-import Foundation
-import NotificationsFeature
-import SharedModels
-import SnapshotTesting
-import SwiftUI
-import XCTest
-
-#if os(iOS)
-    @available(macOS, unavailable)
-    final class NotificationsSnapshotsTests: XCTestCase {
-        @MainActor func test_notifications_loading() {
-            let notificationsView = NotificationsView(
-                store: .init(
-                    initialState: .init(),
-                    reducer: {
-                        NotificationReducer()
-                    }
-                )
-            )
-
-            assertSnapshot(
-                of: notificationsView,
-                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-            )
-        }
-
-        @MainActor
-        func test_notifications_empty() async {
-            let emptyNotifications = [SharedModels.Notification]()
-
-            let store = StoreOf<NotificationReducer>(
-                initialState: .init(
-                    itemsState: NotificationReducer.ItemsState.empty,
-                    notifications: emptyNotifications
-                ),
-                reducer: {
-                    NotificationReducer()
-                        .dependency(\.analyticsClient.sendAnalytics, { _ in })
-                }
-            )
-
-            let notificationsView = NotificationsView(store: store)
-
-            assertSnapshot(
-                of: notificationsView,
-                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-            )
-        }
-
-        @MainActor
-        func test_notifications_loaded() async {
-            let notifications: [SharedModels.Notification] = [
-                .init(
-                    senderName: "KidEight",
-                    senderImageURLString: "https://picsum.photos/200/300",
-                    date: "9 Jul 2021, 11:34 PM"
-                ),
-                .init(
-                    senderName: "Rotation ahsdjkashdkjashdjksahdjksahdjkashdkj",
-                    senderImageURLString: "https://picsum.photos/200/300",
-                    date: "19 Jul 2020, 11:34 PM"
-                ),
-            ]
-
-            let store = StoreOf<NotificationReducer>(
-                initialState: .init(
-                    itemsState: NotificationReducer.ItemsState.loaded,
-                    notifications: notifications
-                ),
-                reducer: {
-                    NotificationReducer()
-                }
-            )
-
-            let notificationsView = NotificationsView(store: store)
-
-            assertSnapshot(
-                of: notificationsView,
-                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-            )
-        }
-
-    }
-#endif
+//            assertSnapshot(
+//                of: notificationsView,
+//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+//            )
+//        }
+//
+//        @MainActor
+//        func test_notifications_empty() async {
+//            let emptyNotifications = [SharedModels.Notification]()
+//
+//            let store = StoreOf<NotificationReducer>(
+//                initialState: .init(
+//                    itemsState: NotificationReducer.ItemsState.empty,
+//                    notifications: emptyNotifications
+//                ),
+//                reducer: {
+//                    NotificationReducer()
+//                        .dependency(\.analyticsClient.sendAnalytics, { _ in })
+//                }
+//            )
+//
+//            let notificationsView = NotificationsView(store: store)
+//
+//            assertSnapshot(
+//                of: notificationsView,
+//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+//            )
+//        }
+//
+//        @MainActor
+//        func test_notifications_loaded() async {
+//            let notifications: [SharedModels.Notification] = [
+//                .init(
+//                    senderName: "KidEight",
+//                    senderImageURLString: "https://picsum.photos/200/300",
+//                    date: "9 Jul 2021, 11:34 PM"
+//                ),
+//                .init(
+//                    senderName: "Rotation ahsdjkashdkjashdjksahdjksahdjkashdkj",
+//                    senderImageURLString: "https://picsum.photos/200/300",
+//                    date: "19 Jul 2020, 11:34 PM"
+//                ),
+//            ]
+//
+//            let store = StoreOf<NotificationReducer>(
+//                initialState: .init(
+//                    itemsState: NotificationReducer.ItemsState.loaded,
+//                    notifications: notifications
+//                ),
+//                reducer: {
+//                    NotificationReducer()
+//                }
+//            )
+//
+//            let notificationsView = NotificationsView(store: store)
+//
+//            assertSnapshot(
+//                of: notificationsView,
+//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+//            )
+//        }
+//
+//    }
+//#endif
