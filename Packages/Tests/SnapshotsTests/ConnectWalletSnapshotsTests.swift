7f8278b3a6846df2bf14874d0fc39a988667b939
Hilal Hakkani <hilal.hakani@360mea.com>
2024-11-03 11:18:01 +0200

    Added tests back

--- Packages/Tests/SnapshotsTests/ConnectWalletSnapshotsTests.swift
+++ Packages/Tests/SnapshotsTests/ConnectWalletSnapshotsTests.swift
@@ -1,163 +1,163 @@
-//import ComposableArchitecture
-//import ConnectWalletFeature
-//import Foundation
-//import SnapshotTesting
-//import SwiftUI
-//import XCTest
-//
-//#if os(iOS)
-//    @available(macOS, unavailable)
-//    final class ConnectWalletSnapshotsTests: XCTestCase {
-//        @MainActor
-//        func test_connectWallet_light() {
-//            let connectWalletView = ConnectingWalletView(
-//                store: .init(
-//                    initialState: .init(wallet: .rainbow),
-//                    reducer: {
-//                        ConnectingWalletViewReducer()
-//                    },
-//                    withDependencies: {
-//                       // $0.analyticsClient = .consoleLogger
-//                        $0.device = .current
-//                        $0.encode = .liveValue
-//                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    }
-//                )
-//            )
-//            .environment(\.colorScheme, .light)
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//
-//        @MainActor
-//        func test_connectWallet_dark() {
-//            let connectWalletView = ConnectingWalletView(
-//                store: .init(
-//                    initialState: .init(wallet: .rainbow),
-//                    reducer: {
-//                        ConnectingWalletViewReducer()
-//                    },
-//                    withDependencies: {
-//                      //  $0.analyticsClient = .consoleLogger
-//                        $0.device = .current
-//                        $0.encode = .liveValue
-//                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    }
-//                )
-//            )
-//            .environment(\.colorScheme, .dark)
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//
-//        @MainActor
-//        func test_connectWallet_alert_light() {
-//            let store = Store(
-//                initialState: ConnectWalletReducer.State(),
-//                reducer: {
-//                    ConnectWalletReducer()
-//                },
-//                withDependencies: {
-//                   // $0.analyticsClient = .consoleLogger
-//                    $0.device = .current
-//                    $0.encode = .liveValue
-//                    $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    $0.analyticsClient.sendAnalytics = { _ in  }
-//                }
-//            )
-//
-//            let connectWalletView = ConnectWalletView(store: store)
-//                .environment(\.colorScheme, .light)
-//
-//            store.send(.view(.onButtonSelect(.coinbase)))
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//
-//        @MainActor
-//        func test_connectWallet_alert_dark() {
-//            let store = Store(
-//                initialState: ConnectWalletReducer.State(),
-//                reducer: {
-//                    ConnectWalletReducer()
-//                },
-//                withDependencies: {
-//                   // $0.analyticsClient = .consoleLogger
-//                    $0.device = .current
-//                    $0.encode = .liveValue
-//                    $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    $0.analyticsClient.sendAnalytics = { _ in  }
-//                }
-//            )
-//
-//            let connectWalletView = ConnectWalletView(store: store)
-//                .environment(\.colorScheme, .dark)
-//
-//            store.send(.view(.onButtonSelect(.coinbase)))
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//
-//        @MainActor
-//        func test_connectingWallet_light() {
-//
-//            let connectWalletView = ConnectWalletView(
-//                store: .init(
-//                    initialState: .init(),
-//                    reducer: {
-//                        ConnectWalletReducer()
-//                    },
-//                    withDependencies: {
-//                       // $0.analyticsClient = .consoleLogger
-//                        $0.device = .current
-//                        $0.encode = .liveValue
-//                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    }
-//                )
-//            )
-//            .environment(\.colorScheme, .light)
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//
-//        @MainActor
-//        func test_connectingWallet_dark() {
-//
-//            let connectWalletView = ConnectWalletView(
-//                store: .init(
-//                    initialState: .init(),
-//                    reducer: {
-//                        ConnectWalletReducer()
-//                    },
-//                    withDependencies: {
-//                        //$0.analyticsClient = .consoleLogger
-//                        $0.device = .current
-//                        $0.encode = .liveValue
-//                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
-//                    }
-//                )
-//            )
-//            .environment(\.colorScheme, .dark)
-//
-//            assertSnapshot(
-//                of: connectWalletView,
-//                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
-//            )
-//        }
-//    }
-//#endif
+import ComposableArchitecture
+import ConnectWalletFeature
+import Foundation
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+#if os(iOS)
+    @available(macOS, unavailable)
+    final class ConnectWalletSnapshotsTests: XCTestCase {
+        @MainActor
+        func test_connectWallet_light() {
+            let connectWalletView = ConnectingWalletView(
+                store: .init(
+                    initialState: .init(wallet: .rainbow),
+                    reducer: {
+                        ConnectingWalletViewReducer()
+                    },
+                    withDependencies: {
+                       // $0.analyticsClient = .consoleLogger
+                        $0.device = .current
+                        $0.encode = .liveValue
+                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    }
+                )
+            )
+            .environment(\.colorScheme, .light)
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+
+        @MainActor
+        func test_connectWallet_dark() {
+            let connectWalletView = ConnectingWalletView(
+                store: .init(
+                    initialState: .init(wallet: .rainbow),
+                    reducer: {
+                        ConnectingWalletViewReducer()
+                    },
+                    withDependencies: {
+                      //  $0.analyticsClient = .consoleLogger
+                        $0.device = .current
+                        $0.encode = .liveValue
+                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    }
+                )
+            )
+            .environment(\.colorScheme, .dark)
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+
+        @MainActor
+        func test_connectWallet_alert_light() {
+            let store = Store(
+                initialState: ConnectWalletReducer.State(),
+                reducer: {
+                    ConnectWalletReducer()
+                },
+                withDependencies: {
+                   // $0.analyticsClient = .consoleLogger
+                    $0.device = .current
+                    $0.encode = .liveValue
+                    $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    $0.analyticsClient.sendAnalytics = { _ in  }
+                }
+            )
+
+            let connectWalletView = ConnectWalletView(store: store)
+                .environment(\.colorScheme, .light)
+
+            store.send(.view(.onButtonSelect(.coinbase)))
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+
+        @MainActor
+        func test_connectWallet_alert_dark() {
+            let store = Store(
+                initialState: ConnectWalletReducer.State(),
+                reducer: {
+                    ConnectWalletReducer()
+                },
+                withDependencies: {
+                   // $0.analyticsClient = .consoleLogger
+                    $0.device = .current
+                    $0.encode = .liveValue
+                    $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    $0.analyticsClient.sendAnalytics = { _ in  }
+                }
+            )
+
+            let connectWalletView = ConnectWalletView(store: store)
+                .environment(\.colorScheme, .dark)
+
+            store.send(.view(.onButtonSelect(.coinbase)))
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+
+        @MainActor
+        func test_connectingWallet_light() {
+
+            let connectWalletView = ConnectWalletView(
+                store: .init(
+                    initialState: .init(),
+                    reducer: {
+                        ConnectWalletReducer()
+                    },
+                    withDependencies: {
+                       // $0.analyticsClient = .consoleLogger
+                        $0.device = .current
+                        $0.encode = .liveValue
+                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    }
+                )
+            )
+            .environment(\.colorScheme, .light)
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+
+        @MainActor
+        func test_connectingWallet_dark() {
+
+            let connectWalletView = ConnectWalletView(
+                store: .init(
+                    initialState: .init(),
+                    reducer: {
+                        ConnectWalletReducer()
+                    },
+                    withDependencies: {
+                        //$0.analyticsClient = .consoleLogger
+                        $0.device = .current
+                        $0.encode = .liveValue
+                        $0.apiClient.connectWallet.connectWallet = { _, _ in .mock1 }
+                    }
+                )
+            )
+            .environment(\.colorScheme, .dark)
+
+            assertSnapshot(
+                of: connectWalletView,
+                as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe))
+            )
+        }
+    }
+#endif
--- Packages/Tests/SnapshotsTests/OnboardingSnapshotsTests.swift
+++ Packages/Tests/SnapshotsTests/OnboardingSnapshotsTests.swift
@@ -1,111 +1,111 @@
-//import ComposableArchitecture
-//import Foundation
-//import OnboardingFeature
-//import SnapshotTesting
-//import XCTest
-//
-//#if os(iOS)
-//  @available(macOS, unavailable)
-//  final class OnboardingSnapshotsTests: XCTestCase {
-//    @MainActor
-//    func testpage1_light() {
-//        let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step2)) {
-//          OnboardingViewReducer()
-//        }
-//
-//      let onboardingView = OnboardingView(store: store)
-//        .environment(\.colorScheme, .light)
-//
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//
-//    @MainActor
-//    func testpage2_light() {
-//      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step2)) {
-//        OnboardingViewReducer()
-//      }
-//
-//      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//
-//      @MainActor
-//    func testpage3_light() {
-//      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step3)) {
-//        OnboardingViewReducer()
-//      }
-//
-//      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//
-//      @MainActor
-//    func testpage4_light() {
-//      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step4)) {
-//        OnboardingViewReducer()
-//      }
-//
-//      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//
-//      @MainActor
-//    func testpage1_dark() {
-//      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step1)) {
-//        OnboardingViewReducer()
-//      }
-//
-//      let onboardingView = OnboardingView(store: store)
-//        .environment(\.colorScheme, .dark)
-//
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//
-//    @MainActor
-//    func testpage2_dark() {
-//      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step2)) {
-//        OnboardingViewReducer()
-//      }
-//
-//      let onboardingView = OnboardingView(store: store)
-//        .environment(\.colorScheme, .dark)
-//
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//      @MainActor
-//    func testpage3_dark() {
-//      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step3)) {
-//        OnboardingViewReducer()
-//      }
-//      let onboardingView = OnboardingView(store: store)
-//        .environment(\.colorScheme, .dark)
-//
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//      @MainActor
-//    func testpage4_dark() {
-//      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step4)) {
-//        OnboardingViewReducer()
-//      }
-//      let onboardingView = OnboardingView(store: store)
-//        .environment(\.colorScheme, .dark)
-//      assertSnapshot(
-//        of: onboardingView,
-//        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
-//    }
-//  }
-//#endif
+import ComposableArchitecture
+import Foundation
+import OnboardingFeature
+import SnapshotTesting
+import XCTest
+
+#if os(iOS)
+  @available(macOS, unavailable)
+  final class OnboardingSnapshotsTests: XCTestCase {
+    @MainActor
+    func testpage1_light() {
+        let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step2)) {
+          OnboardingViewReducer()
+        }
+
+      let onboardingView = OnboardingView(store: store)
+        .environment(\.colorScheme, .light)
+
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+
+    @MainActor
+    func testpage2_light() {
+      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step2)) {
+        OnboardingViewReducer()
+      }
+
+      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+
+      @MainActor
+    func testpage3_light() {
+      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step3)) {
+        OnboardingViewReducer()
+      }
+
+      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+
+      @MainActor
+    func testpage4_light() {
+      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step4)) {
+        OnboardingViewReducer()
+      }
+
+      let onboardingView = OnboardingView(store: store).environment(\.colorScheme, .light)
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+
+      @MainActor
+    func testpage1_dark() {
+      let store: StoreOf<OnboardingViewReducer> = .init(initialState: .init(currentStep: .step1)) {
+        OnboardingViewReducer()
+      }
+
+      let onboardingView = OnboardingView(store: store)
+        .environment(\.colorScheme, .dark)
+
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+
+    @MainActor
+    func testpage2_dark() {
+      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step2)) {
+        OnboardingViewReducer()
+      }
+
+      let onboardingView = OnboardingView(store: store)
+        .environment(\.colorScheme, .dark)
+
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+      @MainActor
+    func testpage3_dark() {
+      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step3)) {
+        OnboardingViewReducer()
+      }
+      let onboardingView = OnboardingView(store: store)
+        .environment(\.colorScheme, .dark)
+
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+      @MainActor
+    func testpage4_dark() {
+      let store = StoreOf<OnboardingViewReducer>.init(initialState: .init(currentStep: .step4)) {
+        OnboardingViewReducer()
+      }
+      let onboardingView = OnboardingView(store: store)
+        .environment(\.colorScheme, .dark)
+      assertSnapshot(
+        of: onboardingView,
+        as: .image(perceptualPrecision: precision, layout: .device(config: .iPhoneSe)))
+    }
+  }
+#endif
